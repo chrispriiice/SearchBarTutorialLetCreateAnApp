f6bebed8e85fb57871f463be933bff22cda4907d
Chris Price <christopher.r.price@gmail.com>
2020-04-09 15:19:31 -0400

    Initial commit.

---
+++ SearchBarTutorial-LetCreateAnApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchBarTutorial-LetCreateAnApp
+//
+//  Created by Chris Price on 4/9/20.
+//  Copyright © 2020 com.chrispriiice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
